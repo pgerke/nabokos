4bf28b97263b361aa211e25dbe5835227023c885
Philip Gerke <philip@Philips-MBP.fritz.box>
2019-09-24 22:54:41 +0200

    test #3: Implemented unit tests

--- src/app/level.service.spec.ts
+++ src/app/level.service.spec.ts
@@ -1,12 +1,99 @@
 import { TestBed } from '@angular/core/testing';
-
 import { LevelService } from './level.service';
 
 describe('LevelService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: LevelService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.get(LevelService);
+  });
 
   it('should be created', () => {
-    const service: LevelService = TestBed.get(LevelService);
     expect(service).toBeTruthy();
+  });
+
+  it('should get current level', () => {
+    const refLevel = `   ###
+  ## # ####
+ ##  ###  #
+## $      #
+#   @$ #  #
+### $###  #
+  #  #..  #
+ ## ##.# ##
+ #      ##
+ #     ##
+ #######`;
+    const level = service.getCurrentLevel();
+    expect(level.name).toBe('Level 1');
+    expect(level.serialized).toBe(refLevel);
+  });
+
+  it('should get next and previous level', () => {
+    const refLevel1 = `   ###
+  ## # ####
+ ##  ###  #
+## $      #
+#   @$ #  #
+### $###  #
+  #  #..  #
+ ## ##.# ##
+ #      ##
+ #     ##
+ #######`;
+    const refLevel2 = ` ## #####
+## ## . #
+# ## $. #
+ ## $   #
+## $@ ###
+# $  ##
+#.. ## ##
+#   # ##
+##### #^`;
+    let level = service.getNextLevel();
+    expect(level.name).toBe('Level 2');
+    expect(level.serialized).toBe(refLevel2);
+    level = service.getPreviousLevel();
+    expect(level.name).toBe('Level 1');
+    expect(level.serialized).toBe(refLevel1);
+  });
+
+  it('should get next and previous level with wrap around', () => {
+    const refLevel1 = `   ###
+  ## # ####
+ ##  ###  #
+## $      #
+#   @$ #  #
+### $###  #
+  #  #..  #
+ ## ##.# ##
+ #      ##
+ #     ##
+ #######`;
+    const refLevel2 = `##############################
+# .$ ## $. ## .$ ## $. ## .$ #
+#$  .  .  $##$  .  .  $##$  .#
+#.  $##$  .  .  $##$  .  .  $#
+# $. ## .$ ## $. ## .$ ## $. #
+### #  # ##  ## #  # ##  ## ##
+### #  # ## #  . # # ##  ## ##
+# $. ## .$ #   $  # .$ ## $. #
+#.  $##$  . .$##  #$  .  .  $#
+#$  .  .  $#  @#$. .  $##$  .#
+# .$ ## $. #  $   # $. ## .$ #
+## ##  ## # # .  # ## #  # ###
+## ##  ## #  # ##  ## #  # ###
+# .$ ## $. ## .$ ## $. ## .$ #
+#$  .  .  $##$  .  .  $##$  .#
+#.  $##$  .  .  $##$  .  .  $#
+# $. ## .$ ## $. ## .$ ## $. #
+##############################`;
+    let level = service.getPreviousLevel();
+    expect(level.name).toBe('Level 355');
+    expect(level.serialized).toBe(refLevel2);
+    level = service.getNextLevel();
+    expect(level.name).toBe('Level 1');
+    expect(level.serialized).toBe(refLevel1);
   });
 });
