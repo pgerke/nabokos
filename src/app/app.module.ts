d6487b519c4cec8510f53b0c4c18811db4333118
Philip Gerke <philip@Philips-MBP.fritz.box>
2019-09-25 21:21:38 +0200

    feat #9: implemented service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TileComponent } from './tile/tile.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -9,7 +11,8 @@ import { TileComponent } from './tile/tile.component';
     TileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
